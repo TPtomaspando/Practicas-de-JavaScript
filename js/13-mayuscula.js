23d7d89b2af514483e71327e086d789d05eaefb6
Tomas Pando <tomaspando11@gmail.com>
2024-10-14 10:40:56 -0300

    realizamos un ejercicio en el que pedimos un texto y lo mostramos en mayuscula

---
+++ js/13-mayuscula.js
@@ -0,0 +1,6 @@
+/*13- Realiza un script que pida un texto y lo muestre en mayúsculas.
+ */
+
+const mayuscula = prompt("ingrese un texto");
+
+document.write("su texto en mayuscula se ve asi: " + mayuscula.toUpperCase());
